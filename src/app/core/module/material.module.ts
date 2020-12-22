698e62f9070d33c3c3c0be20b52d1a1547c5991c
KartikPatelOfficial <patelkartik1910@gmail.com>
2020-12-23 00:29:44 +0530

    Add: Users table

--- src/app/core/module/material.module.ts
+++ src/app/core/module/material.module.ts
@@ -5,21 +5,12 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSortModule} from '@angular/material/sort';
 import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatCardModule} from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatNativeDateModule} from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSelectModule} from '@angular/material/select';
 import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatListModule} from '@angular/material/list';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatTreeModule} from '@angular/material/tree';
 
 const modules = [
   CommonModule,
@@ -30,19 +21,10 @@ const modules = [
   MatDialogModule,
   MatButtonModule,
   MatIconModule,
-  MatToolbarModule,
-  MatSidenavModule,
   MatCardModule,
   MatSelectModule,
-  MatChipsModule,
-  MatSnackBarModule,
   MatTooltipModule,
   MatNativeDateModule,
-  MatDatepickerModule,
-  MatListModule,
-  MatTreeModule,
-  MatGridListModule,
-  MatExpansionModule,
 ];
 
 @NgModule({
