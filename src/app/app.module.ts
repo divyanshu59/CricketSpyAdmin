0b954f6ebdf5e5c513c0c00198536df5923b6d7e
divyanshu59 <greatdivyanshu59@gmail.com>
2020-12-23 19:07:33 +0530

    Fix: Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsersComponent } from './ui/pages/users/users.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSortModule } from '@angular/material/sort';
-import { MatInputModule } from '@angular/material/input';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { AddMatchComponent } from './ui/pages/add-match/add-match.component';
-import { MatButtonModule } from '@angular/material/button';
 import { RoutingModule } from './core/module/routing.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireStorageModule } from '@angular/fire/storage';
@@ -21,25 +13,25 @@ import { OwlDateTimeModule, OwlNativeDateTimeModule } from '@danielmoncada/angul
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './ui/pages/login/login.component';
 import { ToolbarComponent } from './ui/widget/toolbar/toolbar.component';
+import { SidenavComponent } from './ui/widget/sidenav/sidenav.component';
+import { MaterialModule } from './core/module/material.module';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     AddMatchComponent,
     LoginComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatTableModule,
     RouterModule,
-    MatIconModule,
     RoutingModule,
-    MatSortModule,
-    MatInputModule,
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
     FormsModule,
@@ -54,8 +46,6 @@ import { ToolbarComponent } from './ui/widget/toolbar/toolbar.component';
       measurementId: 'G-DM3EJSN9N5'
     }),
     AngularFireStorageModule,
-    MatTooltipModule,
-    MatButtonModule,
     RouterModule,
     RouterModule
   ],
